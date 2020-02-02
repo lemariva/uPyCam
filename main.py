c74dacfd170682a00c392b7b6281a2b41e6d3953
Mauro Riva <lemariva@gmail.com>
2020-02-02 21:24:41 +0100

    added try and except to reset in case of sd/cam problems

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ from ftp import ftpserver
 led = machine.Pin(4, machine.Pin.OUT)
 camera.init()
 # sd mount
-spi = machine.SPI(1, baudrate=100000, phase=0, polarity=0, sck=machine.Pin(14), mosi=machine.Pin(15), miso=machine.Pin(2)) 
-sd = sdcard.SDCard(spi, machine.Pin(13))
-os.mount(sd, '/sd')
-os.listdir('/')
+try:
+    spi = machine.SPI(1, baudrate=100000, phase=0, polarity=0, sck=machine.Pin(14), mosi=machine.Pin(15), miso=machine.Pin(2)) 
+    sd = sdcard.SDCard(spi, machine.Pin(13))
+    os.mount(sd, '/sd')
+    os.listdir('/')
+except:
+    machine.reset()
 
 # ntp sync for date
 ntptime.settime()
@@ -24,22 +27,28 @@ rtc = machine.RTC()
 u = ftpserver()
 u.start_thread()
 
+error_counter = 0
 while True:
-    # prepare for photo
-    led.value(1)
-    led.value(0)
-    time.sleep_ms(100)
+    try:
+        # prepare for photo
+        led.value(1)
+        led.value(0)
+        time.sleep_ms(100)
 
-    # take photo
-    buf = camera.capture()
-    time.sleep_ms(100)
-    # save photo
-    timestamp = rtc.datetime()
-    time_str = '%4d%02d%02d%02d%02d%02d' %(timestamp[0], timestamp[1], timestamp[2], timestamp[4], timestamp[5], timestamp[6])
-    f = open('sd/'+time_str+'.jpg', 'w')
-    f.write(buf)
-    time.sleep_ms(100)
-    f.close()
+        # take photo
+        buf = camera.capture()
+        time.sleep_ms(100)
+        # save photo
+        timestamp = rtc.datetime()
+        time_str = '%4d%02d%02d%02d%02d%02d' %(timestamp[0], timestamp[1], timestamp[2], timestamp[4], timestamp[5], timestamp[6])
+        f = open('sd/'+time_str+'.jpg', 'w')
+        f.write(buf)
+        time.sleep_ms(100)
+        f.close()
 
-    # sleep
-    time.sleep_ms(1000)
+        # sleep
+        time.sleep_ms(2000)
+    except:
+        error_counter = error_counter + 1
+        if error_counter > 10:
+            machine.reset()
