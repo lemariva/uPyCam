fc6da66beaabd67588971f5eb54afe09e4b09387
Mauro Riva <lemariva@gmail.com>
2020-06-22 23:15:51 +0200

    (feat) application updated

--- boot.py
+++ boot.py
@@ -1,20 +1,32 @@
 # boot.py
-# wlan access
-ssid_ = <wlan-ssid>
-wp2_pass = <wlan-password>
+import config
+import network
+import utime
+import ntptime
 
 ## ftp access
 #from ftp import ftpserver
 
 def do_connect():
-    import network
     sta_if = network.WLAN(network.STA_IF)
+    start = utime.time()
+    timed_out = False
+
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect(ssid_, wp2_pass)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
+        sta_if.connect(config.wifi_config["ssid"], config.wifi_config["password"])
+        while not sta_if.isconnected() and \
+            not timed_out:        
+            if utime.time() - start >= 20:
+                timed_out = True
+            else:
+                pass
+
+    if sta_if.isconnected():
+        ntptime.settime()
+        print('network config:', sta_if.ifconfig())
+    else: 
+        print('internet not available')
 
 do_connect()
--- webcam.py
+++ webcam.py
@@ -4,14 +4,14 @@ import picoweb
 import machine
 import time
 import uasyncio as asyncio
+from config import *
 
-led = machine.Pin(4, machine.Pin.OUT)
+led = machine.Pin(app_config['led'], machine.Pin.OUT)
 app = picoweb.WebApp('app')
 
 import ulogging as logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('app')
-
 
 @app.route('/')
 def index(req, resp):
@@ -27,10 +27,19 @@ def index(req, resp):
         camera.deinit()
         await asyncio.sleep(1)
         # If we fail to init, return a 503
-        if (not camera.init()):
-            yield from picoweb.start_response(resp, status=503)
-            yield from resp.awrite('ERROR: Failed to initialise camera\r\n\r\n')
-            return
+
+        if app_config['camera'] == 'ESP32-CAM':
+            if(not camera.init(0, format=camera.JPEG)):  # ESP32-CAM
+                yield from picoweb.start_response(resp, status=503)
+                yield from resp.awrite('ERROR: Failed to initialise camera\r\n\r\n')
+                return
+
+        elif app_config['camera'] == 'M5CAMERA':
+            if (not camera.init(0, d0=32, d1=35, d2=34, d3=5, d4=39, d5=18, d6=36, d7=19,
+                    href=26, vsync=25, reset=15, sioc=23, siod=22, xclk=27, pclk=21)):   #M5CAMERA  
+                yield from picoweb.start_response(resp, status=503)
+                yield from resp.awrite('ERROR: Failed to initialise camera\r\n\r\n')
+                return
 
     # wait for sensor to start and focus before capturing image
     await asyncio.sleep(2)
